a1f79dc77d32022279ef66ae43ac54a9b4990aab
Oleksandr Shekera <oleksandrshekera@gmail.com>
2022-08-16 20:27:19 +0300

    Added UseQueryTrackingBehavior

--- WebAPI/DataAccessLayer/EF/Context.cs
+++ WebAPI/DataAccessLayer/EF/Context.cs
@@ -10,10 +10,16 @@ namespace DataAccessLayer.EF
     {
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
-
+        public Context()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EmployeeDB;Trusted_Connection=True;");
+            optionsBuilder
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EmployeeDB;Trusted_Connection=True;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
     }
 }
